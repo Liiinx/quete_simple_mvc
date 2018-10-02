ba13e5437fe6fbb24526e8451d12da9f37a694cf
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2018-10-02 10:52:06 +0200

    MVC2

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -1,14 +1,19 @@
 <?php
+
+namespace Model;
 // src/Model/ItemManager.php
 require __DIR__ . '/../../app/db.php';
 
-// récupération de tous les items
-function selectAllItems() :array
-{
-    $pdo = new \PDO(DSN, USER, PASS);
-    $query = "SELECT * FROM item";
-    $res = $pdo->query($query);
-    return $res->fetchAll();
-}
 
+class ItemManager {
+
+    // récupération de tous les items
+    public function selectAllItems() :array
+    {
+        $pdo = new \PDO(DSN, USER, PASS);
+        $query = "SELECT * FROM item";
+        $res = $pdo->query($query);
+        return $res->fetchAll();
+    }
+}
 ?>
