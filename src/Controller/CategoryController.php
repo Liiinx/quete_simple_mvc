e1891e0fc3f573cddeadd73c3b4b4f8f46844f19
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2018-10-11 18:03:46 +0200

    twwwwwwwwwwiiiiiiiigggg

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,20 +9,34 @@
 namespace Controller;
 // src/Controller/ItemController.php
 use Model\CategoryManager;
+//use View\View;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 
 class CategoryController {
+
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
 
     // controller pour toutes les categories
     public function categories() {
         $categoryManager = new CategoryManager();
         $categories =  $categoryManager->selectAllCategories();
-        require __DIR__ . '/../View/categories.php';
+        //require __DIR__ . '/../View/categories.html.twig';
+        return $this->twig->render('categories.html.twig', ['categories' => $categories]);
     }
     // controller pour une category
     public function category($id) {
         $itemManager = new CategoryManager();
         $category = $itemManager->selectOneCategory($id);
-        require __DIR__ . '/../View/showCategory.php';
+        //require __DIR__ . '/../View/showCategory.html.twig';
+        return $this->twig->render('showCategory.html.twig', ['category' => $category]);
+
     }
 }
--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,33 +4,50 @@ namespace Controller;
 // src/Controller/ItemController.php
 use Model\ItemManager;
 
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
 //require __DIR__ . '/../Model/ItemManager.php';
 
 class ItemController{
 
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
+
     public function index() {
         $itemManager = new ItemManager();
         $items =  $itemManager->selectAllItems();
-        require __DIR__ . '/../View/item.php';
+        //require __DIR__ . '/../View/item.html.twig';
+
+        // appel de la vue avec method twig
+        return $this->twig->render('item.html.twig', ['items' => $items]);
+
     }
 
     public function show($id) {
         $itemManager = new ItemManager();
         $item =  $itemManager->selectOneItem($id);
-        require __DIR__ . '/../View/showItem.php';
+        //require __DIR__ . '/../View/showItem.html.twig';
+        return $this->twig->render('showItem.html.twig', ['item' => $item]);
+
     }
     /*
     // controller pour toutes les categories
     public function categories() {
         $itemManager = new ItemManager();
         $categories =  $itemManager->selectAllCategories();
-        require __DIR__ . '/../View/categories.php';
+        require __DIR__ . '/../View/categories.html.twig';
     }
     // controller pour une category
     public function category($id) {
         $itemManager = new ItemManager();
         $category = $itemManager->selectOneCategory($id);
-        require __DIR__ . '/../View/showCategory.php';
+        require __DIR__ . '/../View/showCategory.html.twig';
     } */
 }
 ?>
